888b3ed671f2eabb833c3f66d89aba7d76de14a9
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-10-16 23:58:34 +0300

    added css-layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 $(function(){
 
+  $(".range-slider").ionRangeSlider({
+    hide_min_max: true,
+    hide_from_to: true,
+    onStart: function (data) {
+   
+    },
+    onChange: function (data) {
+      $("#"+data.input[0].id+"-label").html(data.from);
+    },
+    onFinish: function (data) {
+       
+    },
+    onUpdate: function (data) {
+    
+    }
+  
+  });
+
 });
